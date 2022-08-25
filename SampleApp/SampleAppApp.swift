f767133cc275f92bbea0da14cb59f9f4a47586a2
Alex Trott <hello@alextrott.me>
2022-08-25 13:03:56 +0100

    Adds base files

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Alex Trott on 25/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
